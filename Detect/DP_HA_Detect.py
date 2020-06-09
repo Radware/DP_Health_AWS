6daf08003f9a5bdddf66263f9fe5c1a1cd37ddd4
Valentine Gutkin <admin@networklabs.info>
2020-06-09 15:56:02 +0300

    Update DP_HA_Detect.py
    Split to functions 
    Change run mode to Async
    Set sleep between tests to minimum 10 seconds
    Set SNMP timeout to 1 sec.
    Use Instance TAG 'DefenceProInstance' value as the DP Name

--- Detect/DP_HA_Detect.py
+++ Detect/DP_HA_Detect.py
@@ -13,6 +13,7 @@ async def get_host_list():
     client = boto3.client('ec2', 'us-east-2')
     # Locate instances with "DefenceProInstance" tag
     for tag in client.describe_tags(Filters=[{'Name': 'key','Values': ['DefenceProInstance']}])["Tags"]:
+        print(tag)
         # Get instance information 
         instance_info = client.describe_instances(InstanceIds=[tag["ResourceId"]])
         try:
@@ -20,20 +21,19 @@ async def get_host_list():
             for interface in instance_info["Reservations"][0]["Instances"][0]["NetworkInterfaces"]:
                 if "Attachment" in interface and "DeviceIndex" in interface["Attachment"] and interface["Attachment"]["DeviceIndex"] == 1:
                     print("Found DP with MGMT IP = " + interface["PrivateIpAddress"])
-                    task_list.append(asyncio.create_task(run_test(interface["PrivateIpAddress"])))
+                    task_list.append(asyncio.create_task(run_SNMP_test(interface["PrivateIpAddress"])))
         except Exception as e:
             print("[ERROR] unable to find network interface in instance info! instance ID: %s, error: %s " % (tag["ResourceId"],e))
     
     for task in task_list:
         await task
 
-async def run_test(host):
-    endtime = int(time.time()) + 10
+async def run_SNMP_test(host):
     oid = '1.3.6.1.4.1.89.35.1.112.0'
     community = 'public'
-    repeat = 90
-
+    repeat = 5
     for i in range(repeat):
+        endtime = int(time.time()) + 10
         cmdGen = cmdgen.CommandGenerator()
         
         errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
@@ -49,5 +49,5 @@ async def run_test(host):
             else:
                 value=str(varBinds[0][1])
                 print("{ \"Description\": \"DefensePro Health Keep-Alive CPU query\", \"DefensePro IP\": \"%s\", \"value\": %s }" % (host, value))
-        if endtime>time.time():
-            await asyncio.sleep(endtime-time.time())
+        if endtime > int(time.time()):
+            await asyncio.sleep(endtime-int(time.time()))
