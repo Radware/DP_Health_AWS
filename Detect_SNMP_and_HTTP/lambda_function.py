dac35a82052f0bb8b11ba6d1b8132763757ef32d
Valentine Gutkin <ValentineG@radware.com>
2020-08-10 15:42:00 +0300

    fix missing module

--- Detect_SNMP_and_HTTP/lambda_function.py
+++ Detect_SNMP_and_HTTP/lambda_function.py
@@ -3,6 +3,7 @@ import time
 import boto3
 import asyncio
 import os
+import aiohttp
 
 def lambda_handler(event, context):
     print ("[INFO] Starting DP Health Detector Lambda!")
@@ -22,19 +23,28 @@ async def get_host_list(context):
             if instance_info["Reservations"][0]["Instances"][0]["VpcId"] != os.environ["VpcId"]:
                 print (f'[INFO] found a DefensePro Instance in a different VPC, instance ID = {instance_info["Reservations"][0]["Instances"][0]["InstanceId"]}')
                 continue
+            
+            try:
+                for instance_tag in instance_info["Reservations"][0]["Instances"][0]['Tags']:
+                    if instance_tag["Key"] == "DefenseProHealthURL":
+                        url = instance_tag["Value"]
+            except Exception as e:
+                print(f"[ERROR] unable to find DefenseProHealthURL! instance ID: {tag['ResourceId']}, error: {e} ")
+                continue
+
             # Loop through the interfaces searching for ETH1 (MGMT)
             for interface in instance_info["Reservations"][0]["Instances"][0]["NetworkInterfaces"]:
                 if "Attachment" in interface and "DeviceIndex" in interface["Attachment"] and interface["Attachment"]["DeviceIndex"] == 1:
                     print("[INFO] Found DP with MGMT IP = " + interface["PrivateIpAddress"])
                     # Create a task for running the tests
-                    task_list.append(asyncio.create_task(run_test(context,interface["PrivateIpAddress"], tag['Value'])))
+                    task_list.append(asyncio.create_task(run_test(context,interface["PrivateIpAddress"], tag['Value'], url)))
         except Exception as e:
             print(f"[ERROR] unable to find network interface in instance info! instance ID: {tag['ResourceId']}, error: {e} ")
     # execute the test tasks 
     for task in task_list:
         await task
 
-async def run_test(context, host, dpName):
+async def run_test(context, host, dpName, url):
     oid = '.1.3.6.1.4.1.89.35.1.112.0'
     community = 'public'
     cloudwatch = boto3.client('cloudwatch')
@@ -62,6 +72,16 @@ async def run_test(context, host, dpName):
                 }], Namespace = f'{dpName}_CPU' )
 
         else:
+            response = cloudwatch.put_metric_data(
+                MetricData = [ 
+                    { 'MetricName': 'DP_KeepAlive_Timeouts', 'Dimensions': [
+                        { 'Name': 'DefensePro_Name', 'Value': dpName },
+                        { 'Name': 'DefensePro_IP', 'Value': host }
+                    ],
+                'Unit': 'None',
+                'Value': 0
+                }], Namespace = f'{dpName}_CPU' )
+
             if errorStatus:
                 print('%s at %s' % ( errorStatus.prettyPrint(),errorIndex and varBinds[int(errorIndex)-1] or '?') )
             else:
@@ -75,7 +95,25 @@ async def run_test(context, host, dpName):
                     'Unit': 'None',
                     'Value': int(varBinds[0][1])
                     }], Namespace = f'{dpName}_CPU' )
+
         # Run HTTP Test
+        async with aiohttp.ClientSession() as session:
+            scode = await fetch_HTTP_Response(session, url)
+            print(f'{{ "Description": "DefensePro Health Keep-Alive CPU query", "Name": "{dpName}", "DefensePro IP": "{host}", "Tested URL": "{url}" "HTTP_Result": {scode} }}')
+            if scode in [200]:
+                httpres = 0
+            else:
+                httpres = 10
+
+            response = cloudwatch.put_metric_data(
+                MetricData = [ 
+                    { 'MetricName': 'DP_KeepAlive_Results', 'Dimensions': [
+                        { 'Name': 'DefensePro_Name', 'Value': dpName },
+                        { 'Name': 'DefensePro_IP', 'Value': host }
+                    ],
+                'Unit': 'None',
+                'Value': httpres
+                }], Namespace = f'{dpName}_HTTP' )
 
         if endtime > int(time.time()) and context.get_remaining_time_in_millis() > 10000:
             await asyncio.sleep(endtime-int(time.time()))
